a4c7c5a655bb65e84b68f3b130005bf50e4a23c5
Alan MacLeod <amacleod@rskorbital.com>
2017-09-26 10:59:34 +0700

    .

--- bench.js
+++ bench.js
@@ -1,6 +1,5 @@
 var bitmeddler = require('./main')
 
-
 var range = process.argv[2] || 10000;
 
 // bitmeddler setup
@@ -13,14 +12,12 @@ var naive_next, naive_rand;
 var naive_size = range;
 while (naive_size >= naive_i) naive_ary.push(naive_i++);
 
-
 function bm(bm_size)
 {
     var bm_next = 1;
     while (bm_next)
       bm_next = bmed.next();
 }
-
 
 function naive(naive_size)
 {
